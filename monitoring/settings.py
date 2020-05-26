09dfb6fdbf1dc529a200955c6c9e4a8d10205e0e
Mario-Hurtado <ma.hurtados@uniandes.edu.co>
2020-05-25 21:43:46 -0500

    Es en serio?

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
+DATABASES = {
     #'default': {
       # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
      #   'NAME': 'canemdb',
