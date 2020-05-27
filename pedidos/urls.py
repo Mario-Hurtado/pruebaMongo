cb469a642eeb7680bc4f97f497b938372128ffac
Mario-Hurtado <ma.hurtados@uniandes.edu.co>
2020-05-26 19:44:55 -0500

    jfnrjnf

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import *
 from django.conf.urls import url
 
 urlpatterns = [
 
-    url(r'^listaPedidos/', views.getPedidos)
+    url(r'^listaPedidos/', getPedidos)
 
 ]
